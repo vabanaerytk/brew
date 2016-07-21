vabanaerytk/brew

commit 95d4ddb33d3ecd2af1c480e5f4a5e335d11ecbd5

formula_installer: fix option-with-value handling
When passing formula options with value, e.g. `--with-qt=5`, to the
child process responsible for building a formula, `ARGV.value` would be
invoked with `nil`. Handle this more elegantly (no change in behavior).

For consistency, use a regular expression adapted from `Options.create`
instead of the somewhat bogus one used before.

Library/Homebrew/formula_installer.rb
@@ -538,9 +538,9 @@ class FormulaInstaller
     end
 
     formula.options.each do |opt|
-      name  = opt.name[/\A(.+)=\z$/, 1]
-      value = ARGV.value(name)
-      args << "--#{name}=#{value}" if name && value
+      name = opt.name[/^([^=])+=$/, 1]
+      value = ARGV.value(name) if name
+      args << "--#{name}=#{value}" if value
     end
 
     args

commit f44e51d9898ac672844771bbe4d2b532636f57e7

FormulaInstaller#sanitized_ARGV_options: fix regex
This fixes a regression caused by 95d4ddb33d3ecd2af1c480e5f4a5e335d11ecbd5,
where a misplaced `+` cause the regex only catch one character.

Fixes https://github.com/Homebrew/homebrew-science/issues/3847.

Library/Homebrew/formula_installer.rb
@@ -535,7 +535,7 @@ class FormulaInstaller
     end
 
     formula.options.each do |opt|
-      name = opt.name[/^([^=])+=$/, 1]
+      name = opt.name[/^([^=]+)=$/, 1]
       value = ARGV.value(name) if name
       args << "--#{name}=#{value}" if value
     end