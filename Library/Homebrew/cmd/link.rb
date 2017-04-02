vabanaerytk/brew

commit af1c7bd25e174b6066e8c96cd380ab086acf8d9b

link: tell users of `brew link --force` about opt.
If people are force-linking keg-only things they should probably be told
that they can add the opt prefix to their PATH instead.

Library/Homebrew/cmd/link.rb
@@ -44,6 +44,7 @@ module Homebrew
       elsif keg_only && !ARGV.force?
         opoo "#{keg.name} is keg-only and must be linked with --force"
         puts "Note that doing so can interfere with building software."
+        puts_keg_only_path_message(keg)
         next
       elsif mode.dry_run && mode.overwrite
         puts "Would remove:"
@@ -53,6 +54,7 @@ module Homebrew
       elsif mode.dry_run
         puts "Would link:"
         keg.link(mode)
+        puts_keg_only_path_message(keg) if keg_only
 
         next
       end
@@ -69,8 +71,23 @@ module Homebrew
         else
           puts "#{n} symlinks created"
         end
+
+        if keg_only && !ARGV.homebrew_developer?
+          puts_keg_only_path_message(keg)
+        end
       end
     end
+  end
+
+  def puts_keg_only_path_message(keg)
+    bin = keg/"bin"
+    sbin = keg/"sbin"
+    return if !bin.directory? && !sbin.directory?
+
+    opt = HOMEBREW_PREFIX/"opt/#{keg.name}"
+    puts "\nIf you need to have this software first in your PATH instead consider running:"
+    puts "  #{Utils::Shell.prepend_path_in_shell_profile(opt)}/bin"  if bin.directory?
+    puts "  #{Utils::Shell.prepend_path_in_shell_profile(opt)}/sbin" if sbin.directory?
   end
 
   def keg_only?(rack)

commit 9b92af9fb8f8294c501c5f0eb0b32e13f621503a

Fix the 'export PATH' message in `link` for a keg-only formula
Commit af1c7bd25e174b6066e8c96cd380ab086acf8d9b introduced the message,
but it printed the wrong path, e.g. for `brew link sqlite`:

```
If you need to have this software first in your PATH instead consider running:
  echo 'export PATH="/usr/local/opt/sqlite:$PATH"' >> ~/.zshrc/bin
```

where `/bin` is appended at the end, but should be inserted before
`:$PATH`: `echo 'export PATH="/usr/local/opt/sqlite/bin:$PATH"' >> ~/.zshrc`.
This patch fixes that and updates a test to verify it.

Library/Homebrew/cmd/link.rb
@@ -86,8 +86,8 @@ module Homebrew
 
     opt = HOMEBREW_PREFIX/"opt/#{keg.name}"
     puts "\nIf you need to have this software first in your PATH instead consider running:"
-    puts "  #{Utils::Shell.prepend_path_in_shell_profile(opt)}/bin"  if bin.directory?
-    puts "  #{Utils::Shell.prepend_path_in_shell_profile(opt)}/sbin" if sbin.directory?
+    puts "  #{Utils::Shell.prepend_path_in_shell_profile(opt/"bin")}"  if bin.directory?
+    puts "  #{Utils::Shell.prepend_path_in_shell_profile(opt/"sbin")}" if sbin.directory?
   end
 
   def keg_only?(rack)

Library/Homebrew/test/cmd/link_spec.rb
@@ -48,9 +48,11 @@ describe "brew link", :integration_test do
       expect { brew "install", "testball1" }.to be_a_success
     end
 
-    expect { brew "link", "testball1" }
+    expect { brew "link", "testball1", "SHELL" => "/bin/zsh" }
       .to output(/testball1 is keg-only/).to_stderr
-      .and output(/Note that doing so can interfere with building software\./).to_stdout
+      .and output(a_string_matching(/Note that doing so can interfere with building software\./)
+        .and(matching("If you need to have this software first in your PATH instead consider running:")
+        .and(including("echo 'export PATH=\"#{HOMEBREW_PREFIX}/opt/testball1/bin:$PATH\"' >> ~/.zshrc")))).to_stdout
       .and be_a_success
   end
 end