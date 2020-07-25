vabanaerytk/brew

commit f6b69ab12476da7b8cc99345452b92554fd4a2e6

Refactor `Bintray` not to depend on `args`.

Library/Homebrew/bintray.rb
@@ -10,30 +10,27 @@ class Bintray
   end
 
   def inspect
-    "#<Bintray: user=#{@bintray_user} org=#{@bintray_org} key=***>"
+    "#<Bintray: org=#{@bintray_org}>"
   end
 
-  def initialize(user: ENV["HOMEBREW_BINTRAY_USER"], key: ENV["HOMEBREW_BINTRAY_KEY"], org: "homebrew", clear: true)
-    @bintray_user = user
-    @bintray_key = key
+  def initialize(org: "homebrew")
     @bintray_org = org
-
-    if !@bintray_user || !@bintray_key
-      unless Homebrew.args.dry_run?
-        raise UsageError, "Missing HOMEBREW_BINTRAY_USER or HOMEBREW_BINTRAY_KEY variables!"
-      end
-    end
 
     raise UsageError, "Must set a Bintray organisation!" unless @bintray_org
 
     ENV["HOMEBREW_FORCE_HOMEBREW_ON_LINUX"] = "1" if @bintray_org == "homebrew" && !OS.mac?
-
-    ENV.delete "HOMEBREW_BINTRAY_KEY" if clear
   end
 
   def open_api(url, *extra_curl_args, auth: true)
     args = extra_curl_args
-    args += ["--user", "#{@bintray_user}:#{@bintray_key}"] if auth
+
+    if auth
+      raise UsageError, "HOMEBREW_BINTRAY_USER is unset." unless (user = EnvConfig.bintray_user)
+      raise UsageError, "HOMEBREW_BINTRAY_KEY is unset." unless (key = EnvConfig.bintray_key)
+
+      args += ["--user", "#{user}:#{key}"]
+    end
+
     curl(*args, url,
          show_output: Homebrew.args.verbose?,
          secrets:     @bintray_key)

Library/Homebrew/dev-cmd/pr-upload.rb
@@ -48,13 +48,9 @@ module Homebrew
 
     if args.dry_run?
       puts "brew #{bottle_args.join " "}"
-    else
-      safe_system HOMEBREW_BREW_FILE, *bottle_args
-    end
-
-    if args.dry_run?
       puts "Upload bottles described by these JSON files to Bintray:\n  #{Dir["*.json"].join("\n  ")}"
     else
+      safe_system HOMEBREW_BREW_FILE, *bottle_args
       bintray.upload_bottle_json(Dir["*.json"],
                                  publish_package: !args.no_publish?,
                                  warn_on_error:   args.warn_on_upload_failure?)

Library/Homebrew/test/bintray_spec.rb
@@ -3,7 +3,13 @@
 require "bintray"
 
 describe Bintray, :needs_network do
-  bintray = described_class.new(user: "BrewTestBot", key: "deadbeef", org: "homebrew")
+  subject(:bintray) { described_class.new(org: "homebrew") }
+
+  before do
+    ENV["HOMEBREW_BINTRAY_USER"] = "BrewTestBot"
+    ENV["HOMEBREW_BINTRAY_KEY"] = "deadbeef"
+  end
+
   describe "::file_published?" do
     it "detects a published file" do
       results = bintray.file_published?(repo: "bottles", remote_file: "hello-2.10.catalina.bottle.tar.gz")

commit ff2e711325ef389819056e76939c53d3f628c079

bintray: Fix "uninitialized constant EnvConfig" errors
- This fix was suggested by Sorbet when I ran `HOMEBREW_SORBET=1 bundle
  exec srb tc` on the latest `master` while playing around with the
  latest changes post-GSoC meeting.
- Then I noticed it was actually a bug, introduced in
  f6b69ab12476da7b8cc99345452b92554fd4a2e6, found by us not being able to
  publish bottles for [this build of the `n`
  formula](https://github.com/Homebrew/homebrew-core/runs/910309641?check_suite_focus=true)
  in https://github.com/Homebrew/homebrew-core/pull/58606:

```
[master 31d32307bd] n: update 6.7.0 bottle.
 1 file changed, 3 insertions(+), 3 deletions(-)
curl: (22) The requested URL returned error: 404 Not Found
Error: uninitialized constant Bintray::EnvConfig
/home/linuxbrew/.linuxbrew/Homebrew/Library/Homebrew/bintray.rb:28:in `open_api'
/home/linuxbrew/.linuxbrew/Homebrew/Library/Homebrew/bintray.rb:43:in `upload'
/home/linuxbrew/.linuxbrew/Homebrew/Library/Homebrew/bintray.rb:186:in `block (2 levels) in upload_bottle_json'
/home/linuxbrew/.linuxbrew/Homebrew/Library/Homebrew/bintray.rb:158:in `each'
/home/linuxbrew/.linuxbrew/Homebrew/Library/Homebrew/bintray.rb:158:in `block in upload_bottle_json'
/home/linuxbrew/.linuxbrew/Homebrew/Library/Homebrew/bintray.rb:153:in `each'
/home/linuxbrew/.linuxbrew/Homebrew/Library/Homebrew/bintray.rb:153:in `upload_bottle_json'
/home/linuxbrew/.linuxbrew/Homebrew/Library/Homebrew/dev-cmd/pr-upload.rb:54:in `pr_upload'
/home/linuxbrew/.linuxbrew/Homebrew/Library/Homebrew/brew.rb:111:in `<main>'
Error: Failure while executing; `/home/linuxbrew/.linuxbrew/bin/brew pr-upload --verbose --bintray-org=homebrew` exited with 1.
```

Library/Homebrew/bintray.rb
@@ -25,8 +25,8 @@ class Bintray
     args = extra_curl_args
 
     if auth
-      raise UsageError, "HOMEBREW_BINTRAY_USER is unset." unless (user = EnvConfig.bintray_user)
-      raise UsageError, "HOMEBREW_BINTRAY_KEY is unset." unless (key = EnvConfig.bintray_key)
+      raise UsageError, "HOMEBREW_BINTRAY_USER is unset." unless (user = Homebrew::EnvConfig.bintray_user)
+      raise UsageError, "HOMEBREW_BINTRAY_KEY is unset." unless (key = Homebrew::EnvConfig.bintray_key)
 
       args += ["--user", "#{user}:#{key}"]
     end